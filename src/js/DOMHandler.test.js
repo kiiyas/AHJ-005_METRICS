d97b62dfc61c63824f54538959073ac20b0fa87d
Kiyas <kiiyas@yandex.ru>
2022-03-21 22:12:22 +0500

    trying fixing bugs

--- src/js/DOMHandler.test.js
+++ src/js/DOMHandler.test.js
@@ -1,9 +1,9 @@
 import DOMpopHandler from './DOMHandler';
 
-// const container = document.querySelector('.container');
+const container = document.querySelector('.container');
 const addPopover = new DOMpopHandler();
 
 test('Method DOMpopHandler.create() should create a DIV-element in DOM', () => {
   addPopover.create(); // ? не может выполнить это, отсюда не видит DOM
-  expect(true).toBe(true);
+  expect(container.lastElementChild.className).toBe('btn-popover-container');
 });
